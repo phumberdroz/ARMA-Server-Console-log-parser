9f56bdd2763498743137029edeb00653bcd35145
Pierre Humberdroz <p.humberdroz@gmail.com>
2017-12-11 07:34:35 +0100

    store

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var filefinder = require("./filefinder");
 var lineReader = require('line-reader');
 var mysql      = require('mysql');
 var patterns   = require('node-grok').loadDefaultSync();
+var filepatterns   = require('./patterns')
 
 console.log(filefinder.fromDir);
 files = [];
@@ -9,24 +10,17 @@ filefinder.fromDir('../logs',/server_console\.log$/,function(filename){
     files.push(filename);
 });
 
-var connectpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} \\(%{IPV4:connectip}:%{NUMBER}\\) connected';
-var disconnectpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} disconnected';
-var beguidpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} - GUID: %{GREEDYDATA:beguid}'
-var steamidpattern = '%{TIME:timestamp} Player %{GREEDYDATA:username} connected \\(id=%{NUMBER:steamid}\\).'
-var messagepattern = '%{TIME:timestamp} BattlEye Server: \\(%{WORD:channel}\\) %{GREEDYDATA:username}: %{GREEDYDATA:message}'
-var datepattern = '%{GREEDYDATA}%{DATE_EU:date}/%{DATA:time}/%{GREEDYDATA}'
-var timepattern = '%{TIME:timestamp}%{GREEDYDATA}';
+var connect     = patterns.createPattern(filepatterns.connectpattern);
+var disconnect  = patterns.createPattern(filepatterns.disconnectpattern);
+var beguid      = patterns.createPattern(filepatterns.beguidpattern);
+var steamid     = patterns.createPattern(filepatterns.steamidpattern);
+var message     = patterns.createPattern(filepatterns.messagepattern);
+var date        = patterns.createPattern(filepatterns.datepattern);
+var time        = patterns.createPattern(filepatterns.timepattern);
 
-
-var connect     = patterns.createPattern(connectpattern);
-var disconnect  = patterns.createPattern(disconnectpattern);
-var beguid      = patterns.createPattern(beguidpattern);
-var steamid     = patterns.createPattern(steamidpattern);
-var message     = patterns.createPattern(messagepattern);
-var date        = patterns.createPattern(datepattern);
-var time        = patterns.createPattern(timepattern);
 sessions = [];
 counter=0
+adjusted=0;
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -43,38 +37,26 @@ function callback () {
       steamid: sessions[i].steamid,
       beguid: sessions[i].beguid,
       ip: sessions[i].connectip,
-      connect: new Date(sessions[i].connecttime).toISOString().slice(0, 19).replace('T', ' '),
-      disconnect: new Date(sessions[i].disconnecttime).toISOString().slice(0, 19).replace('T', ' '),
       username: sessions[i].username,
     }, function (error, results, fields) {
       if (error) throw error;
+      if (results.insertId%100==0) console.log("current insert " + results.insertId);
+
     });
   }
 }
-timestamps=[]
-for (var i = 0; i < files.length; i++) {
-  timestamp = date.parseSync(files[i]);
-  timestamp.date =timestamp.date.split(".")[2]+"."+(timestamp.date.split(".")[1]-1)+"."+timestamp.date.split(".")[0];
-  timestamps.push(timestamp)
-}
+
 for (var x = 0; x < files.length; x++) {
-  lasttime = "";
   lineReader.eachLine(files[x], function(line, last) {
     if (connect.parseSync(line)) {
       tmp = connect.parseSync(line);
       sessions.unshift({
         username: tmp.username,
-        connecttime: new Date(timestamps[counter].date + " " + tmp.timestamp),
-        disconnecttime: "",
         connectid: tmp.connectid,
         connectip: tmp.connectip,
         steamid: "",
-        beguid: ""
+        beguid: "",
       });
-    } else if (disconnect.parseSync(line)) {
-      tmp = disconnect.parseSync(line);
-      const session = sessions.findIndex((session) => session.connectid === tmp.connectid);
-      sessions[session].disconnecttime=new Date(timestamps[counter].date + " " + tmp.timestamp);
     } else if (beguid.parseSync(line)) {
       tmp = beguid.parseSync(line);
       const session = sessions.findIndex((session) => session.connectid === tmp.connectid);
@@ -87,28 +69,13 @@ for (var x = 0; x < files.length; x++) {
       }
     }
 
-    if (time.parseSync(line)) {
-        lasttime = time.parseSync(line).timestamp
-    }
-
     if (last){
-      for (var i = 0; i < sessions.length; i++) {
-        if (sessions[i].disconnecttime== ""){
-          sessions[i].disconnecttime=new Date(timestamps[counter].date + " " + lasttime);
-        }
-        fileDate=new Date(timestamps[counter].date + " " + timestamp.time.replace(/_/g, ":"))
-        connecttime=new Date(sessions[i].connecttime);
-        if (fileDate<connecttime){
-          sessions[i].connecttime = connecttime.setDate(connecttime.getDate() - 1);
-        }
-        disconnecttime=new Date(sessions[i].disconnecttime);
-        if (fileDate<disconnecttime){
-          sessions[i].disconnecttime = disconnecttime.setDate(disconnecttime.getDate() - 1);
-        }
-      }
-      console.log("finished" + files[counter])
-      console.log("current sessions " + sessions.length)
+
       counter++
+
+      console.log("finished " + files[counter])
+      console.log("current sessions " + sessions.length)
+      console.log("current adjustments " + adjusted)
       if(counter === files.length) {
         callback();
       }
---
+++ patterns.js
@@ -0,0 +1,7 @@
+exports.connectpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} \\(%{IPV4:connectip}:%{NUMBER}\\) connected';
+exports.disconnectpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} disconnected';
+exports.beguidpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} - GUID: %{GREEDYDATA:beguid}'
+exports.steamidpattern = '%{TIME:timestamp} Player %{GREEDYDATA:username} connected \\(id=%{NUMBER:steamid}\\).'
+exports.messagepattern = '%{TIME:timestamp} BattlEye Server: \\(%{WORD:channel}\\) %{GREEDYDATA:username}: %{GREEDYDATA:message}'
+exports.datepattern = '%{GREEDYDATA}/%{DATE_EU:date}/%{DATA:time}/%{GREEDYDATA}'
+exports.timepattern = '%{TIME:timestamp}%{GREEDYDATA}';
