24f206db3d3786762b6be892daa75c9b5fd2c096
Pierre Humberdroz <p.humberdroz@gmail.com>
2017-12-17 22:14:11 +0100

    update pattern

--- patterns.js
+++ patterns.js
@@ -1,6 +1,6 @@
 exports.connectpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} \\(%{IPV4:connectip}:%{NUMBER}\\) connected';
 exports.disconnectpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} disconnected';
-exports.disconnectbynamepattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} disconnected';
+exports.disconnectbynamepattern = '%{TIME:timestamp} Player %{GREEDYDATA:username} disconnected';
 exports.beguidpattern = '%{TIME:timestamp} BattlEye Server: Player #%{NUMBER:connectid} %{GREEDYDATA:username} - GUID: %{GREEDYDATA:beguid}'
 exports.steamidpattern = '%{TIME:timestamp} Player %{GREEDYDATA:username} connected \\(id=%{NUMBER:steamid}\\).'
 exports.messagepattern = '%{TIME:timestamp} BattlEye Server: \\(%{WORD:channel}\\) %{DATA:username}: %{GREEDYDATA:message}'
